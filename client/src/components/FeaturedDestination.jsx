ecfdb9934d17dbadb1529d6d3f9a662397af3555
Shahd Ahmed <moftahshahd2@gmail.com>
2025-06-16 00:08:07 +0300

    change lable

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -15,7 +15,7 @@ const FeaturedDestination = () => {
                 ))}
             </div>
             <button onClick={() => { navigate('/rooms'); scrollTo(0, 0) }} className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>
-                View All Destinations
+                View All Rooms
             </button>
         </div>
     )
